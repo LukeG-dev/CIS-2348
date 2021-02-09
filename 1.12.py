d9ed58f4f5ccedf7cde839976a91d4aa819734af
LukeG-dev <77751707+LukeG-dev@users.noreply.github.com>
2021-02-09 17:16:07 -0600

    Delete 1.12.py

--- 1.12.py
+++
@@ -1,5 +0,0 @@
-# Luke Gilin 1216992
-userNum = int(input())
-userNumSquared = userNum ** 2 # Bug here; fix it when instructed
-
-print(userNumSquared) # Output formatting issue here; fix it when instructed

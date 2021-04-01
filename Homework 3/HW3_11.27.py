bb1568d0b1e80b282890911a3ca392b098d5a3f9
LukeG-dev <77751707+LukeG-dev@users.noreply.github.com>
2021-03-31 22:16:32 -0500

    Add files via upload

---
+++ Homework 3/HW3_11.27.py
@@ -0,0 +1,15 @@
+#  Luke Gilin
+#  1216992
+
+if __name__ == '__main__':
+   playerOneNumber = input('Enter player 1''s jersey number:')
+   playerOneRating = input('Enter player 1''s rating:')
+   playerTwoNumber = input('Enter player 2''s jersey number:')
+   playerTwoRating = input('Enter player 2''s rating:')
+   playerThreeNumber = input('Enter player 3''s jersey number:')
+   playerThreeRating = input('Enter player 3''s rating:')
+   playerFourNumber = input('Enter player 4''s jersey number:')
+   playerFourRating = input('Enter player 4''s rating:')
+   playerFiveNumber = input('Enter player 5''s jersey number:')
+   playerFiveRating = input('Enter player 5''s rating:')
+   

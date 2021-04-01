1af272925b46d0da522300d15cdc27e84534b976
LukeG-dev <77751707+LukeG-dev@users.noreply.github.com>
2021-03-31 22:46:49 -0500

    Add files via upload

--- Homework 3/HW3_11.27.py
+++ Homework 3/HW3_11.27.py
@@ -2,14 +2,31 @@
 #  1216992
 
 if __name__ == '__main__':
-   playerOneNumber = input('Enter player 1''s jersey number:')
-   playerOneRating = input('Enter player 1''s rating:')
-   playerTwoNumber = input('Enter player 2''s jersey number:')
-   playerTwoRating = input('Enter player 2''s rating:')
-   playerThreeNumber = input('Enter player 3''s jersey number:')
-   playerThreeRating = input('Enter player 3''s rating:')
-   playerFourNumber = input('Enter player 4''s jersey number:')
-   playerFourRating = input('Enter player 4''s rating:')
-   playerFiveNumber = input('Enter player 5''s jersey number:')
-   playerFiveRating = input('Enter player 5''s rating:')
-   
+    playerOneNumber = int(input("Enter player 1's jersey number:\n"))
+    playerOneRating = int(input("Enter player 1's rating:\n\n"))
+    playerTwoNumber = int(input("Enter player 2's jersey number:\n"))
+    playerTwoRating = int(input("Enter player 2's rating:\n\n"))
+    playerThreeNumber = int(input("Enter player 3's jersey number:\n"))
+    playerThreeRating = int(input("Enter player 3's rating:\n\n"))
+    playerFourNumber = int(input("Enter player 4's jersey number:\n"))
+    playerFourRating = int(input("Enter player 4's rating:\n\n"))
+    playerFiveNumber = int(input("Enter player 5's jersey number:\n"))
+    playerFiveRating = int(input("Enter player 5's rating:\n\n"))
+
+    Players = {playerOneNumber: playerOneRating, playerTwoNumber: playerTwoRating,
+               playerThreeNumber: playerThreeRating, playerFourNumber: playerFourRating,
+               playerFiveNumber: playerFiveRating}
+    print('ROSTER')
+
+    for i in sorted(Players):
+        print('Jersey number: {num}, Rating: {rate}'.format(num=i,rate=Players[i]))
+    print()
+    print('MENU')
+    print('a - Add player')
+    print('d - Remove player')
+    print('u - Update player rating')
+    print('r - Output players above a rating')
+    print('o - Output roster')
+    print('q - Quit\n')
+    input('Choose an option:\n')
+
